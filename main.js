0a4638139a90df0bdf7998a1c2cf044bc27fe898
gizzSaw <gizzsaw@gmail.com>
2022-06-30 03:05:06 +0300

    form

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(function(){
-    $('.bike-slider').slick({
+    $('.bike-slider, .slider__items').slick({
         arrows: false,
         dots: true,
         fade: true,
         autoplay: true,
         autoplaySpeed: 2500,
     });
-})
+});
